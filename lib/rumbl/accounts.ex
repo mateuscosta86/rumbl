7c416d84bcee73b71eab70061e46330da4cfee42
Mateus Costa <info.mateuscosta@gmail.com>
2021-12-07 22:20:33 -0300

    created user schema and changeset function

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,22 +2,19 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Mateus", username: "mateuscosta"},
-      %User{id: "2", name: "Jose", username: "josevalim"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+    Repo.all(User)
   end
 
-  def get_user(id), do: Enum.find(list_users(), fn user -> user.id == id end)
+  def get_user(id), do: Repo.get(User, id)
+  def get_user!(id), do: Repo.get!(User, id)
 
-  def get_user_by(params) do
-    Enum.find(list_users(), fn user ->
-      Enum.all?(params, fn {key, value} -> Map.get(user, key) == value end)
-    end)
+  def get_user_by(params), do: Repo.get_by(User, params)
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
   end
 end
--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -12,4 +12,9 @@ defmodule RumblWeb.UserController do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
   end
+
+  def new(comm, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
 end
